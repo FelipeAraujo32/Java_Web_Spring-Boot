9254a3ba55b34e7aba0f27d52ac65ba2bf4f2374
FelipeAraujo32 <felipecafsx@gmail.com>
2023-10-27 16:28:52 -0300

    Adicionando DELETE

--- javaweb/src/main/java/com/javaweb/javaweb/controllers/UserControllers.java
+++ javaweb/src/main/java/com/javaweb/javaweb/controllers/UserControllers.java
@@ -3,6 +3,7 @@ package com.javaweb.javaweb.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,4 +42,14 @@ public class UserControllers {
         User result = userRepository.save(user);
         return result;
     }
-}
+
+    @DeleteMapping(value = "/{id}")
+        public User delete(@PathVariable Long id){
+            User user = userRepository.findById(id).get();
+            userRepository.delete(user);
+            return user;
+            
+        }
+    }
+
+
--- javaweb/src/main/java/com/javaweb/javaweb/repository/DepartmentRepositor.java
+++
@@ -1,8 +0,0 @@
-package com.javaweb.javaweb.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.javaweb.javaweb.entities.Department;
-
-public interface DepartmentRepositor extends JpaRepository <Department, Long> {
-    
-}
